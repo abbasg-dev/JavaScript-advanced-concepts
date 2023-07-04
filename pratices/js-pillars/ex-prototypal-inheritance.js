a449648b7b075f77edd97e1102bfbaf52fec448b
abbasghaith23 <abbasghaith23@gmail.com>
2023-07-04 05:58:09 -0800

    prototypal-inheritance

---
+++ pratices/js-pillars/ex-prototypal-inheritance.js
@@ -0,0 +1,22 @@
+// Exercise - extend the functionality of a built in object
+
+//#1
+// Date object => to have new method .lastYear() which shows you last year 'YYYY' format.
+Date.prototype.lastYear = function() {
+    return this.getFullYear() - 1;
+}
+// console.log(new Date('1900-10-10').lastYear())
+// '1899'
+
+//#Bonus
+// Modify .map() to print '🎷' at the end of each item.
+Array.prototype.map = function() {
+    let arr = [];
+    for(let i = 0; i < this.length; i++) {
+        arr.push((this[i] + '🎷'))
+    }
+    return arr
+}
+
+console.log([1,2,3].map())
+//1🎷, 2🎷, 3🎷
---
+++ pratices/js-pillars/prototypal-inheritance.js
@@ -0,0 +1,55 @@
+let dragon = {
+    name: 'Tanya',
+    fire: true,
+    fight() {
+        return 5
+    },
+    sing() {
+        if (this.fire) {
+            return `I am ${this.name}, the breather of fire`
+        }
+    }
+}
+
+// console.log(dragon.fight());
+
+let lizard = {
+    name: 'Kiki',
+    fight() {
+        return 1
+    }
+}
+
+const singLizard = dragon.sing.bind(lizard)
+
+// console.log(singLizard());
+
+lizard.__proto__ = dragon;
+
+// console.log(lizard.sing());
+
+// console.log(lizard.fight());
+
+// lizard.dance();
+
+// console.log(lizard.isPrototypeOf(dragon))
+
+for (let prop in lizard) {
+    if (lizard.hasOwnProperty(prop)) {
+        console.log(prop);
+    }
+}
+
+// lizard.haha()
+
+// console.log(lizard.haha)
+
+let human = {
+    mortal: true
+}
+let socrates = Object.create(human)
+socrates.age = 45
+// console.log(socrates.mortal); true
+// console.log(human.isPrototypeOf(socrates)); true
+
+// Only functions have the prototype property
