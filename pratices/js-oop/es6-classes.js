991eb98a3c69a21b8adda1a899bc7ec2fe80c8c6
abbasghaith23 <abbasghaith23@gmail.com>
2023-07-15 04:59:55 -0800

    es6 classes

---
+++ pratices/js-oop/es6-classes.js
@@ -0,0 +1,18 @@
+Object.create()
+// ES6 Class
+class Elf {
+    constructor(name, weapon) {
+        this.name = name;
+        this.weapon = weapon;
+    }
+
+    attack() {
+        return 'attack with ' + this.weapon
+    }
+}
+
+const peter = new Elf('Peter', 'stones')
+console.log(Elf instanceof peter)
+console.log(peter.attack());
+const sam = new Elf('Sam', 'fire')
+console.log(sam.attack())
