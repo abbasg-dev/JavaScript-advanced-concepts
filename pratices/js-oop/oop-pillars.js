22f83bea48fb54aa573b53e2ba4fb74ca45ac577
aghaith <abbasghayth5@gmail.com>
2024-08-29 15:05:22 +0400

    OOP Pillars and polymorphism

---
+++ pratices/js-oop/oop-pillars.js
@@ -0,0 +1,33 @@
+// Encapsulation
+// wrap code into boxes that are related to one another
+// so that these boxes can just interact with each other using the methods and properties that we make available
+// this makes code easier to maintain and more reusable
+// we have all these nice little class packages that we can just use it when ever we want
+
+// Abstraction
+// hiding the complexity from the user that is creating simpler interfaces
+// the idea is here are the methods and properties that you can use.
+// don't worry about everything else. I'll do all the calculations behind the scenes.
+// this reduces complexity because we can just see the methods and understand what this class can do
+// the idea also helps when we have private variables and private methods, something that JavaScript is working towards
+
+// Inheritance
+// Inheriting from other classes, we avoid having to write the same code
+// we also save memory space by having shared methods
+
+// polymorphism
+// the idea is the ability to call the same method on different objects and each object responding in different way.
+// the idea of ability to appear in many forms, that is, we're simply doing method overwriting
+// it has ability to process objects differently depending on their data type or class
+// redefine methods for derived classes and allowing us to re-use some of the functionality but also customize methods to their own objects and classes
+// we can re-use some of the functionality from a superclass to adapt to our own needs.
+
+/*
+    Created Code
+    Clear and understandable
+    easy to extend
+    easy to maintain
+*/
+
+// memory efficient (objects use references instead of cloning and copying)
+// Code dry (one place were creating functionality)
---
+++ pratices/js-oop/polymorphism.js
@@ -0,0 +1,42 @@
+class Character {
+  // base class (super class)
+  constructor(name, weapon) {
+    this.name = name;
+    this.weapon = weapon;
+  }
+
+  attack() {
+    return "attack with " + this.weapon;
+  }
+}
+
+class Elf extends Character {
+  // sub class
+  constructor(name, weapon, type) {
+    super(name, weapon); // it calls the constructor in the base class, super is referring to the super class
+    this.type = type;
+  }
+  attack(cry) {
+    return "attack with " + cry;
+  }
+}
+
+class Ogre extends Character {
+  constructor(name, weapon, color) {
+    super(name, weapon); // Character
+    this.color = color;
+  }
+  attack() {
+    return "arghhhhh";
+  }
+  makeFort() {
+    return "strongest fort in the world made";
+  }
+}
+
+// new keyword to create instances of a class, instance is creating a version of a class
+const dolby = new Elf("Dolby", "cloth", "house");
+
+const shrek = new Ogre("shrek", "club", "green");
+console.log(dolby.attack("weeee")); // attack with weeee
+console.log(shrek.attack()); // arghhhhh
