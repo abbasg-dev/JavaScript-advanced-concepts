1143b1195e5dc0495efd6bd0393ce76bd0041d1e
aghaith <abbasghayth5@gmail.com>
2024-08-30 16:29:45 +0400

    Imperative vs declarative

---
+++ pratices/functional-programming/imperative-vs-declarative.js
@@ -0,0 +1,8 @@
+// Imperative code is code that tells the machine what to do and how to do
+// Declarative code tells it what to do and what should happen
+
+for (let i = 0; i < 1000; i++) {
+  console.log(i);
+}
+
+[1, 2, 3].forEach((item) => console.log(item));
