45d353b66be49aee7f1a666f1f175146229094b9
aghaith <abbasghayth5@gmail.com>
2024-08-30 12:55:21 +0400

    Modify oop pillars

--- pratices/js-oop/oop-pillars.js
+++ pratices/js-oop/oop-pillars.js
@@ -1,26 +1,14 @@
 // Encapsulation
-// wrap code into boxes that are related to one another
-// so that these boxes can just interact with each other using the methods and properties that we make available
-// this makes code easier to maintain and more reusable
-// we have all these nice little class packages that we can just use it when ever we want
+// Group related code into classes to restrict access to internal details, making components easier to manage and reuse.
 
-// Abstraction
-// hiding the complexity from the user that is creating simpler interfaces
-// the idea is here are the methods and properties that you can use.
-// don't worry about everything else. I'll do all the calculations behind the scenes.
-// this reduces complexity because we can just see the methods and understand what this class can do
-// the idea also helps when we have private variables and private methods, something that JavaScript is working towards
+//Abstraction
+// Simplify interfaces by hiding complex logic, exposing only necessary methods and properties to the user.
 
 // Inheritance
-// Inheriting from other classes, we avoid having to write the same code
-// we also save memory space by having shared methods
+// Reuse code across components by inheriting common functionality from parent classes, reducing duplication.
 
-// polymorphism
-// the idea is the ability to call the same method on different objects and each object responding in different way.
-// the idea of ability to appear in many forms, that is, we're simply doing method overwriting
-// it has ability to process objects differently depending on their data type or class
-// redefine methods for derived classes and allowing us to re-use some of the functionality but also customize methods to their own objects and classes
-// we can re-use some of the functionality from a superclass to adapt to our own needs.
+// Polymorphism
+// Enable different objects to respond uniquely to the same method call, allowing flexible and extensible design.
 
 /*
     Created Code
