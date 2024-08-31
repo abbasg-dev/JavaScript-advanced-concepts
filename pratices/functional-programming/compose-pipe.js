095f1c5f3c6972df97da4ee3e1ff7e76f96123b1
aghaith <abbasghayth5@gmail.com>
2024-08-31 15:15:54 +0400

    Compose and pipe

---
+++ pratices/functional-programming/compose-pipe.js
@@ -0,0 +1,15 @@
+// Composability is a system design principle that deals with the relationship between components.
+// Pipe chains functions left-to-right, passing the result of each as input to the next.
+
+// ex - fn1(fn2(fn3(50)));
+
+// compose(fn1, fn2, fn3)(n);
+// pipe(fn3, fn2, fn1)(n);
+
+const compose = (f, g) => (data) => f(g(data));
+const pipe = (f, g) => (data) => g(f(data));
+const multiplyBy3 = (num) => num * 3;
+const makePositive = (num) => Math.abs(num);
+const multiplyBy3AndAbsolute = compose(multiplyBy3, makePositive);
+
+console.log(multiplyBy3AndAbsolute(-50)); // 150
