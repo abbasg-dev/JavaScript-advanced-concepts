d6d078468f6dbae27f15e532a5d22d4ba135d5d2
aghaith <abbasghayth5@gmail.com>
2024-08-31 22:04:52 +0400

    Amazon exercise solution

--- pratices/functional-programming/ex-amazon.js
+++ pratices/functional-programming/ex-amazon.js
@@ -6,6 +6,50 @@ const user = {
   purchases: [],
 };
 
+const amazonHistory = [];
+const compose =
+  (f, g) =>
+  (...args) =>
+    f(g(...args));
+purchaseItem(
+  emptyCart,
+  buyItem,
+  applyTaxToItems,
+  addItemToCart
+)(user, { name: "laptop", price: 200 });
+
+function purchaseItem(...fns) {
+  return fns.reduce(compose);
+}
+
+function addItemToCart(user, item) {
+  amazonHistory.push(user);
+  const updateCart = use.cart.concat(item);
+  return Object.assign({}, use, { cart: updateCart });
+}
+
+function applyTaxToItems(user) {
+  amazonHistory.push(user);
+  const taxRate = 1.3;
+  const updatesCart = cart.map((item) => {
+    return {
+      name: item,
+      price: item.price * taxRate,
+    };
+  });
+  return Object.assign({}.user, { cart: updatedCart });
+}
+
+function buyItem(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { purchases: user.cart });
+}
+
+function emptyCart(user) {
+  amazonHistory.push(user);
+  return Object.assign({}, user, { cart: [] });
+}
+
 // Implement a cart feature:
 // 1. Add items to cart.
 // 2. Add 3% tax to item in cart
