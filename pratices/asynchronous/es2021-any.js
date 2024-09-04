6bc2689a0d2a0ce208e2659c154f5dd6fda7864d
aghaith <abbasghayth5@gmail.com>
2024-09-04 20:47:11 +0400

    any

---
+++ pratices/asynchronous/es2021-any.js
@@ -0,0 +1,17 @@
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("A"), Math.floor(Math.random() * 1000));
+});
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("B"), Math.floor(Math.random() * 1000));
+});
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("C"), Math.floor(Math.random() * 1000));
+});
+
+(async function () {
+  const result = await Promise.any([p1, p2, p3]);
+  console.log(result); // Prints "A", "B" or "C"
+})();
+
+// Promise.any() resolves if any of the supplied promises is resolved.
+// if none of the promises resolve? In that case Promise.any() throws an error!
