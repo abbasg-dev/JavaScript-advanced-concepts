9232ecfdc41929c4b70461f9ef3d152670434a1e
aghaith <abbasghayth5@gmail.com>
2024-12-26 19:28:44 +0400

    Types

---
+++ pratices/js-basics/types.js
@@ -0,0 +1,30 @@
+console.log(3 + 4); //7
+console.log(4 + 5); //9
+console.log(3 * 5); //15
+console.log(12 / 4); //3
+console.log(12 - 4); //8
+console.log((3 + 4) * 2); //14
+console.log(12 % 6); //0
+console.log(12 % 5); //2
+console.log(12 % 9); //3
+console.log(3 + 5); //8
+
+console.log("Abbas"); //Abbas
+console.log("Ghaith"); //Ghaith
+console.log("Hello" + "There!"); //HelloThere!
+console.log("Hello " + "there!"); //Hello there!
+console.log("This isn't very nice"); //This isn't very nice
+console.log(10 + "34"); //1034
+console.log(10 - "3"); //7
+console.log("Hello" - "bye"); //NaN
+
+console.log(true); //true
+console.log(false); //false
+console.log(3 > 2); //true
+console.log(5 > 10); //false
+console.log(5 >= 5); //true
+console.log(5 <= 5); //true
+console.log(3 = 3); //Uncaught ReferenceError
+console.log(3 === 3); //true
+console.log(3 !== 3); //false
+console.log(4 !== 5); //true
