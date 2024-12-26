6775ecdd75c70912e53419538a0cbe7f1b576c58
aghaith <abbasghayth5@gmail.com>
2024-12-26 19:27:19 +0400

    Variables

---
+++ pratices/js-basics/variables.js
@@ -0,0 +1,51 @@
+var abbas = "These pretzels are making me thirsty" + "!";
+
+// console.log(abbas);
+// console.log(3 === 3); // true
+
+// var 3 = 3; // SyntaxError: Unexpected number
+var three = 3;
+// console.log(three); // 3
+
+// var &hello = "hello"; // SyntaxError: Unexpected token '&'
+var _name = "Abbas";
+var $name = "Abbas";
+var firstName = "Abbas";
+
+// prompt();
+// prompt("What is your username?");
+/*
+var first = prompt("enter first number");
+// console.log(first);
+Number(first);
+var second = prompt("enter second number");
+// console.log(second);
+Number(second);
+
+var sum = Number(first) + Number(second);
+// console.log(sum);
+
+// alert("hi there!");
+alert("The sum is:" + sum);
+*/
+
+console.log(3); //3
+console.log(3 + 4); //7
+
+var a = true;
+var b = "hello";
+var c = 1;
+
+console.log(a);
+
+a = "Hello";
+console.log(a);
+
+var a;
+console.log(a);
+
+var b;
+console.log(b);
+
+var password;
+console.log(password); // undefined
