71d4e4297022d7d4bfa8f533100308eb35e63587
aghaith <abbasghayth5@gmail.com>
2024-12-26 21:17:52 +0400

    Conditionals

---
+++ pratices/js-basics/conditionals.js
@@ -0,0 +1,62 @@
+var name = "Abbas";
+
+name = "Ahmad";
+
+var firstName = "Abbas";
+var lastName = "Ghaith";
+
+/*
+
+// if (name === "Abbas") {
+//   alert("Hello Abbas!");
+// }
+
+name = "Susy";
+
+if (name === "Abbas") {
+  alert("Hello Abbas!");
+}
+*/
+
+/*
+if (name === "Abbas") {
+  alert("Hello Abbas!");
+} else {
+  alert("Hmm I don't know you");
+}
+*/
+
+/*
+if (name === "Abbas") {
+  alert("Hi Abbas!");
+} else if (name === "Ahmad") {
+  alert("Hi Ahmad!");
+} else {
+  alert("I don't know you");
+}
+*/
+
+/*
+if (name === "Ahmad" || name === "Amani") {
+  alert("Hi Ahmad or Amani!");
+}
+*/
+
+/*
+if (name === "Ahmad" && name === "Amani") {
+  alert("Hi Ahmad or Amani!");
+}
+*/
+
+/*
+if (firstName === "Abbas" && lastName === "Ghaith") {
+  alert("Hi Abbas Ghaith!");
+}
+*/
+
+if (!(name === "Abbas")) {
+  alert("Hi Abbas!");
+}
+
+console.log(!false); //true
+console.log(!true); //false
