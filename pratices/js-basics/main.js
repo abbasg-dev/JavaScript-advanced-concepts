04a8f31a76ad6d62d0db86b95e42d2061f920dac
aghaith <abbasghayth5@gmail.com>
2024-12-27 00:22:13 +0400

    js in web page

---
+++ pratices/js-basics/main.js
@@ -0,0 +1,11 @@
+console.log(4 + 3);
+
+if (4 + 3 === 7) {
+  console.log("Helloooo!");
+}
+
+console.log("Helloooo!");
+console.log("Helloooo!");
+console.log("Helloooo!");
+console.log("Helloooo!");
+console.log("Helloooo!");
