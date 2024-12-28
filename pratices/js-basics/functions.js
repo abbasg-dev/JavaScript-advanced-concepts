02e4bbc576eb03d8ff792e8b7c5d322fb33d77bb
aghaith <abbasghayth5@gmail.com>
2024-12-28 21:06:41 +0400

    Functions

---
+++ pratices/js-basics/functions.js
@@ -0,0 +1,54 @@
+//prompt();
+//alert("hi there!");
+//console.log("Hello", "how are you?");
+
+function sayHello() {
+  console.log("Hello");
+}
+
+// sayHello();
+
+var sayBye = function () {
+  console.log("Bye");
+};
+
+// sayBye();
+
+function sing(song) {
+  //   console.log("AHHHHHHH");
+  //   console.log("TEEEEEEE");
+  console.log(song);
+}
+
+// sing("laaaaaaa deeeee daaaaaa");
+// sing("hellooooooo");
+// sing(" backstreets back alright");
+
+function sing2() {
+  console.log("laaaaaaa deeeee daaaaaa");
+  console.log("laaaaaa");
+}
+
+// sing2();
+
+function multiply(a, b) {
+  //console.log(a, b);
+  /*
+  if (a > 10 || b > 10) {
+    return "that's too hard";
+  } else {
+    return a * b;
+  }
+  */
+  return a * b;
+}
+
+// const res = multiply(5, 40);
+// console.log(res);
+// alert(multiply(3, 4)); //12
+
+var total = multiply(4, 5);
+alert(total);
+
+// parameters (a, b)
+// arguments (4, 5)
