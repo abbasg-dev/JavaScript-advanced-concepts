318e0ce5201823e9bfb2016106a2ec0201e08789
aghaith <abbasghayth5@gmail.com>
2024-12-29 15:13:29 +0400

    Arrays

---
+++ pratices/js-basics/arrays.js
@@ -0,0 +1,44 @@
+var list = ["tiger", "cat", "bear", "bird"];
+//console.log(list[0]);
+console.log(list);
+//console.log(list[0][2]);
+
+list.shift(); // tiger
+console.log(list);
+
+list.pop(); //bird
+console.log(list);
+
+list.push("elephant");
+console.log(list);
+
+const myList = list.concat(["bee", "deer"]);
+console.log(myList);
+
+var myNewList = myList.concat(["monkey"]);
+
+console.log(myNewList);
+
+list.sort();
+console.log(list);
+
+var numbers = [1, 2, 3, 4];
+var booleans = [true, false, true];
+
+var functionsList = [
+  function apple() {
+    console.log("apple");
+  },
+];
+console.log(functionsList);
+
+var mixedList = [
+  "apples",
+  "3",
+  undefined,
+  true,
+  function apple() {
+    console.log("apples");
+  },
+];
+console.log(mixedList);
