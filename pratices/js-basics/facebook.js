c368b91ae0f0a3e89617af10a1162f0e6cb9994a
aghaith <abbasghayth5@gmail.com>
2024-12-29 20:51:52 +0400

    Complete facebook exercise

--- pratices/js-basics/facebook.js
+++ pratices/js-basics/facebook.js
@@ -3,6 +3,14 @@ var database = [
     username: "abbas",
     password: "supersecret",
   },
+  {
+    username: "sara",
+    password: "123",
+  },
+  {
+    username: "ahmad",
+    password: "777",
+  },
 ];
 
 var newsFeed = [
@@ -14,13 +22,29 @@ var newsFeed = [
     username: "Sally",
     timeline: "Javascript is sooo cool!",
   },
+  {
+    username: "Sara",
+    timeline: "Javascript is preeetyy cool!",
+  },
 ];
 
 var userNamePrompt = prompt("What's your username?");
 var passwordPrompt = prompt("What's your password?");
 
+function isUserValid(username, password) {
+  for (var i = 0; i < database.length; i++) {
+    if (
+      database[i].username === username &&
+      database[i].password === password
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
 function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
+  if (isUserValid(user, pass)) {
     console.log(newsFeed);
   } else {
     alert("Sorry, wrong username and password!");
