b6378ceaede8a7877ad7cbdbeeccd2f93df3edb1
aghaith <abbasghayth5@gmail.com>
2024-12-31 15:23:10 +0400

    es7

---
+++ pratices/js-intermediate/es7.js
@@ -0,0 +1,16 @@
+// includes() It can be used to determine whether an array includes a given element in an elegant way.
+
+const pets = ["cats", "dog", "bat"];
+
+pets.includes("dog"); // true
+pets.includes("bird"); // false
+
+// exponential operator
+
+const square = (x) => x ** 2;
+square(2); // 4
+square(5); // 25
+
+const cube = (y) => y ** 3;
+cube(3); // 27
+cube(4); // 64
